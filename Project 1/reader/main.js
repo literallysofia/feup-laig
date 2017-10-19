d2884918652bbc82ea2cab67df8a45fd42193fbe
julietafrade97 <up201506530@fe.up.pt>
2017-10-19 16:06:53 +0100

    wallv3 + patch

--- Project 1/reader/main.js
+++ Project 1/reader/main.js
@@ -31,7 +31,7 @@ main=function()
 	// get file name provided in URL, e.g. http://localhost/myproj/?file=myfile.xml
 	// or use "demo.xml" as default (assumes files in subfolder "scenes", check MySceneGraph constructor)
 
-	var filename=getUrlVars()['file'] || "test.xml";
+	var filename=getUrlVars()['file'] || "thewallv3.xml";
 
 	// create and load graph, and associate it to scene.
 	// Check console for loading errors
