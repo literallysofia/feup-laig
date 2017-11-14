dbda9c6eccb40efab7693b29c45a154c6f3f695c
julietafrade97 <up201506530@fe.up.pt>
2017-11-14 14:35:17 +0000

    organização do repositório

---
+++ Project_1/Delivery/Project/reader/primitives/MyCircle.js
@@ -0,0 +1,40 @@
+/**
+ * MyCircle
+ * @constructor
+ */
+function MyCircle(scene, slices) {
+  CGFobject.call(this, scene);
+  this.slices = slices;
+  this.initBuffers();
+};
+
+MyCircle.prototype = Object.create(CGFobject.prototype);
+MyCircle.prototype.constructor = MyCircle;
+
+MyCircle.prototype.initBuffers = function() {
+  this.vertices = [];
+  this.indices = [];
+  this.normals = [];
+  this.texCoords = [];
+
+  var div = 2 * Math.PI / this.slices;
+
+  this.vertices.push(0, 0, 0);
+  this.normals.push(0, 0, 1);
+  this.texCoords.push(0.5, 0.5);
+
+  for (var i = 0; i <= this.slices; i++) {
+    this.vertices.push(Math.cos(i * div), Math.sin(i * div), 0);
+    this.normals.push(0, 0, 1);
+    this.texCoords.push(0.5 + 0.5 * Math.cos(i * div), 0.5 + 0.5 * Math.sin(i * div));
+  }
+
+  for (var i = 0; i < this.slices; i++) {
+    this.indices.push(0, i, i + 1);
+  }
+
+  this.indices.push(0, this.slices, 1);
+
+  this.primitiveType = this.scene.gl.TRIANGLES;
+  this.initGLBuffers();
+}
---
+++ Project_1/Delivery/Project/reader/primitives/MyCylinder.js
@@ -0,0 +1,116 @@
+/**
+ * MyCylinder
+ * @constructor
+ */
+function MyCylinder(scene, height, radiusBottom, radiusTop, stacks, slices, layerTop, layerBottom) {
+  CGFobject.call(this, scene);
+
+  this.radiusTop = radiusTop;
+  this.radiusBottom = radiusBottom;
+  this.height = height;
+  this.slices = slices;
+  this.stacks = stacks;
+  this.layerTop = layerTop;
+  this.layerBottom = layerBottom;
+
+  this.layer = new MyCircle(scene, this.slices);
+
+  this.initBuffers();
+};
+
+MyCylinder.prototype = Object.create(CGFobject.prototype);
+MyCylinder.prototype.constructor = MyCylinder;
+
+MyCylinder.prototype.initBuffers = function() {
+  this.vertices = [];
+  this.indices = [];
+  this.normals = [];
+  this.texCoords = [];
+
+  var r = this.radiusBottom;
+  var delta_r = (this.radiusTop - this.radiusBottom) / this.stacks;
+  var delta_rad = 2 * Math.PI / this.slices;
+  var delta_z = this.height / this.stacks;
+  var m = this.height / (this.radiusBottom - this.radiusTop);
+  var maxheight;
+  if (this.radiusBottom > this.radiusTop)
+    maxheight = this.radiusTop * m + this.height;
+  else maxheight = this.radiusBottom * m + this.height;
+  var indice = 0;
+
+  for (var i = 0; i <= this.stacks; i++) {
+    for (var j = 0; j <= this.slices; j++) {
+      this.vertices.push(
+        r * Math.cos(j * delta_rad),
+        r * Math.sin(j * delta_rad),
+        i * delta_z
+      );
+      if (Math.abs(this.radiusBottom - this.radiusTop) < 0.0001) {
+        this.normals.push(
+          Math.cos(j * delta_rad),
+          Math.sin(j * delta_rad),
+          0);
+      } else if (this.radiusBottom > this.radiusTop) {
+        this.normals.push(
+          maxheight * Math.cos(j * delta_rad) / Math.sqrt(Math.pow(this.radiusBottom, 2) + Math.pow(maxheight, 2)),
+          maxheight * Math.sin(j * delta_rad) / Math.sqrt(Math.pow(this.radiusBottom, 2) + Math.pow(maxheight, 2)),
+          this.radiusBottom / Math.sqrt(Math.pow(this.radiusBottom, 2) + Math.pow(maxheight, 2))
+        );
+      } else {
+        this.normals.push(
+          maxheight * Math.cos(j * delta_rad) / Math.sqrt(Math.pow(this.radiusTop, 2) + Math.pow(maxheight, 2)),
+          maxheight * Math.sin(j * delta_rad) / Math.sqrt(Math.pow(this.radiusTop, 2) + Math.pow(maxheight, 2)),
+          this.radiusTop / Math.sqrt(Math.pow(this.radiusTop, 2) + Math.pow(maxheight, 2))
+        );
+      }
+      this.texCoords.push(j / this.slices, i / this.stacks);
+
+    }
+    r = (i + 1) * delta_r + this.radiusBottom;
+  }
+
+  for (var i = 0; i < this.stacks; i++) {
+    for (var j = 0; j < this.slices; j++) {
+      this.indices.push(
+        i * (this.slices + 1) + j,
+        i * (this.slices + 1) + (j + 1),
+        (i + 1) * (this.slices + 1) + (j + 1)
+      );
+      this.indices.push(
+        (i + 1) * (this.slices + 1) + (j + 1),
+        (i + 1) * (this.slices + 1) + j,
+        i * (this.slices + 1) + j
+      );
+
+    }
+  }
+
+  this.primitiveType = this.scene.gl.TRIANGLES;
+  this.initGLBuffers();
+};
+
+
+
+MyCylinder.prototype.display = function() {
+  CGFobject.prototype.display.call(this);
+
+  if (this.layerTop == 1) {
+    this.scene.pushMatrix();
+    this.scene.scale(this.radiusTop, this.radiusTop, 1);
+    this.scene.translate(0, 0, this.height);
+    this.layer.display();
+    this.scene.popMatrix();
+  }
+
+  if (this.layerBottom == 1) {
+    this.scene.pushMatrix();
+    this.scene.scale(this.radiusBottom, this.radiusBottom, 1);
+    this.scene.rotate(Math.PI, 1, 0, 0);
+    this.layer.display();
+    this.scene.popMatrix();
+  }
+};
+
+MyCylinder.prototype.updateTexCoords = function(afS, afT) {
+  this.updateTexCoordsGLBuffers();
+};
